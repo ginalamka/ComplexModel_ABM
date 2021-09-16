197c027e8ad9fe958ba627613834a981f558ab3a
ginalamka <72942258+ginalamka@users.noreply.github.com>
2021-09-16 10:46:47 -0500

    move Source to RunModel.R
    instead of having the source pop as its own function, I moved Source to RunModel.R so that R has an easier time recalling Source for Migrate.R

--- Source/RunModel.R
+++ Source/RunModel.R
@@ -24,10 +24,21 @@ RunModel = function(parameters, r, directory){
   pop[,6] = sample(c(0,1),k,replace=T)    #set allele 1 as either A=1 or a=0
   pop[,7] = sample(c(0,1),k,replace=T)    #set allele 2 as either A=1 or a=0
 
+  #initialize source population 
+  source = matrix(nrow=k, ncol=7)            #each individual gets its own row.. matrix > dataframe
+  colnames(source) <- c("id", "mom", "dad", "age", "sex", "allele1", "allele2") #just to give a better understanding of what these variables are, set names
+  source[,1] = seq(-1000,-1,1)                     #each individual has unique ID name; sequence starting at -1, through -k, with each 1 interation, negative flag for source pop
+  source[,2:3] = -1                           #at this point, we are putting all equal to negative 1 to flag from source pop, and we dont know parents/parents arent in focal pop
+  source[,4] = sample(seq(0,maxage,1),k,replace=T)   #set age between 0 and 4 (source isnt aged, so dont subtract 1); consider if age 0 should be able to migrate
+  source[,5] = sample(c(0,1),k,replace=T)    #each individual assigned male (1) or female (0) #sample from zero k times, with replacements. aka set sex
+  source[,6] = sample(c(0,1),k,replace=T)    #set allele 1 as either A=1 or a=0
+  source[,7] = sample(c(0,1),k,replace=T)    #set allele 2 as either A=1 or a=0
+  
   #create for loop for each time step
   for(y in 1:years){
     pop = AgeUp(pop)
     pop = DeathByAge(pop, maxage)
+    pop = Migrate(pop, source)
     #dead = sample(seq(1,nrow(pop),1),1,replace=F) #take a sample of the sequence 1-all the rows in matrix "pop", take out 1 individual and do not replace it
     #pop = pop[-dead,] #now re-create "pop" with this change
     return (pop)
