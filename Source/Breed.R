f82dda5abc75a88d4286ff35f72d36578e8d83f4
ginalamka <72942258+ginalamka@users.noreply.github.com>
2021-10-19 17:32:51 -0500

    Add necessary notes
    This code is NOT completed, look at the notes for a way to get it working

--- Source/Breed.R
+++ Source/Breed.R
@@ -4,7 +4,13 @@
 ##{THIS STILL DOES NOT RUN}
 #rethink the steps to make this happen
 
-Breed = function(pop, pairs, years){
+Breed = function(pop, pairs, years, numboff, k){
+  #randomize pairs so that all have an opportunity to make babies
+  
+  
+  
+  
+  
   #use for loop to iterate over pairs (?)
   
   #use sample function to randomly select fecundity of each pair
@@ -28,3 +34,15 @@ Breed = function(pop, pairs, years){
   
 }
 #think about adding in a "generation born" and "generation died" columns in pop
+
+"
+BREEDING NOTES FROM JANNA 10/19/2021  
+next steps: make sure to randomize the order of the pairs table before breeding
+then generate fecundity for each pair down th list for a certain number of times to get past the number of offspring needed to reach K
+make sure to buffer by overshooting the number
+then before rturning offspring, drop some until the total number needed
+total offspring/2*fecundity
+check at end of function if below or above K
+make sure to break with an error message if too small
+return offspring and then rbind them to pop
+""
