6a9b774dbb2afa793e988914fdcc2351863b0f68
ginalamka <72942258+ginalamka@users.noreply.github.com>
2021-10-19 17:33:52 -0500

    edit MateChoice.R
    added in notes. this function is not complete and needs to be further edited so that there is randomization. this code is too complex for the simplicity it needs

--- Source/MateChoice.R
+++ Source/MateChoice.R
@@ -1,7 +1,7 @@
 #Mate
 #used for complex model for ABM class
 
-MateChoice = function(pop){
+MateChoice = function(pop, sex){
   #find which sex has more, male or female.
   mean(pop[,'sex']) #<0.5 female, >.5 male
   
@@ -13,9 +13,19 @@ MateChoice = function(pop){
   
   #pair individuals
   pairs <- cbind(pop[pop[,'sex'] == sex, 'id'], mates)
+  
+  #randomize the pairs so all indv have chance of mating
+  rand = sample(1:nrow(pairs),nrow(pairs))
+  pairs[rand, ]
+  
+  pairs <- rand
   colnames(pairs) <- c('mate1','mate2')
   return(pairs)
 }
+#NOTES FROM JANNA 10/19/2021
+#try this flow: get all females that are mature, randomize them, then match them with males
+#that will rearrange this whole function, but can be done
+#the main thing is to randomize the pairs so that offspring generated are from a random pair
 
 #*note, because krats have a promiscuous system, allow replacement so multiple indv can mate more than once (monogamous would be set up different)
 #as set up currently, only the sex with the most indv will be used more than once (i.e. not both sexes with multiple mating events)
