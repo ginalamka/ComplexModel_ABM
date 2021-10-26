4fdbbb3ac719e43f53bfebe6c51c04aa4da09ff5
ginalamka <72942258+ginalamka@users.noreply.github.com>
2021-10-26 16:03:42 -0500

    Update MateChoice.R
    this makes it so that pairs are randomized, plus females are always in the first column while males are always in second.
    
    NOTE- there is still replacement allowed in matings. for the time being, leave this since only a proportion of pairs actually produce offspring

--- Source/MateChoice.R
+++ Source/MateChoice.R
@@ -5,21 +5,25 @@ MateChoice = function(pop, sex){
   #find which sex has more, male or female.
   mean(pop[,'sex']) #<0.5 female, >.5 male
   
-  #turn "sex" into the value of sex with the fewest indv, 0=female, 1=male
-  #sex <- c(0,1)[which.min(tabulate(match(pop[,'sex'], c(0,1))))]
+  #REMOVED###turn "sex" into the value of sex with the fewest indv, 0=female, 1=male
+  #REMOVED###sex <- c(0,1)[which.min(tabulate(match(pop[,'sex'], c(0,1))))]
   
-  #randomly select indv of the most sex with replacement*
-  mates <- sample(pop[pop[,'sex'] != sex, 'id'], tabulate(match(pop[,'sex'], sex)), replace=FALSE) #this means indv can only mate once
+  fem = 0 #this matches males to females by putting the first column always as females
+  
+  #match those of opposite sex with replacement*
+  mates <- sample(pop[pop[,'sex'] != fem, 'id'], tabulate(match(pop[,'sex'], fem)), replace=TRUE) #this means indv can mate more than once
+  #for now, replace = true since sometimes more males than females
+  #note, != means NOT 
   
   #pair individuals
-  pairs <- cbind(pop[pop[,'sex'] == sex, 'id'], mates)
+  pairs <- cbind(pop[pop[,'sex'] == fem, 'id'], mates)
   
   #randomize the pairs so all indv have chance of mating
   rand = sample(1:nrow(pairs),nrow(pairs))
   pairs <- pairs[rand, ]
   
   #pairs <- rand
-  colnames(pairs) <- c('mate1','mate2')
+  colnames(pairs) <- c('mom','dad')
   return(pairs)
 }
 #NOTES FROM JANNA 10/19/2021
