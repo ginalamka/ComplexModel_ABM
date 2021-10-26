af6cfd151cd0752f42f035eb8b1e53b9f5432460
ginalamka <72942258+ginalamka@users.noreply.github.com>
2021-10-26 15:41:54 -0500

    Update MateChoice.R

--- Source/MateChoice.R
+++ Source/MateChoice.R
@@ -6,7 +6,7 @@ MateChoice = function(pop, sex){
   mean(pop[,'sex']) #<0.5 female, >.5 male
   
   #turn "sex" into the value of sex with the fewest indv, 0=female, 1=male
-  sex <- c(0,1)[which.min(tabulate(match(pop[,'sex'], c(0,1))))]
+  #sex <- c(0,1)[which.min(tabulate(match(pop[,'sex'], c(0,1))))]
   
   #randomly select indv of the most sex with replacement*
   mates <- sample(pop[pop[,'sex'] != sex, 'id'], tabulate(match(pop[,'sex'], sex)), replace=FALSE) #this means indv can only mate once
@@ -16,9 +16,9 @@ MateChoice = function(pop, sex){
   
   #randomize the pairs so all indv have chance of mating
   rand = sample(1:nrow(pairs),nrow(pairs))
-  pairs[rand, ]
+  pairs <- pairs[rand, ]
   
-  pairs <- rand
+  #pairs <- rand
   colnames(pairs) <- c('mate1','mate2')
   return(pairs)
 }
