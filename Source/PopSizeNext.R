f70431c28c954980e899322b312e7d9e0f187731
ginalamka <72942258+ginalamka@users.noreply.github.com>
2021-10-27 17:18:52 -0500

    Update PopSizeNext.R
    make sure that the values being passed are integers. there were issues with Nt1 and numboff being doubles, but this should fix the problem

--- Source/PopSizeNext.R
+++ Source/PopSizeNext.R
@@ -12,9 +12,18 @@ PopSizeNext = function(pop, k, r0){
   
   #add Density Independent variance in growth
   Nt1 = round(rnorm(1, Ntt, 1), 0)
+  Nt1 = as.integer(Nt1)
   
   #determine the number of additional offspring to produce
   numboff = Nt1 - Nt
   
+  #for some reason numboff is a double and causing problems
+  #turn numboff into an integer
+  numboff = as.integer(numboff)
+  print(paste("the number of offspring needed is", numboff))
+  print(paste("numboff is an", typeof(numboff)))
+  
   return(numboff)
 }
+
+#check type of object using typeof(numboff), want it to be an integer, not double
