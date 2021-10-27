ba8fd2c5164264e7922d3521c1ed81a5e0cb6a1d
ginalamka <72942258+ginalamka@users.noreply.github.com>
2021-10-27 14:31:19 -0500

    Update MateChoice.R
    add checks to make sure that pairs can be generated

--- Source/MateChoice.R
+++ Source/MateChoice.R
@@ -3,28 +3,36 @@
 
 MateChoice = function(pop, sex){
   #find which sex has more, male or female.
-  mean(pop[,'sex']) #<0.5 female, >.5 male
-  
-  #REMOVED###turn "sex" into the value of sex with the fewest indv, 0=female, 1=male
-  #REMOVED###sex <- c(0,1)[which.min(tabulate(match(pop[,'sex'], c(0,1))))]
-  
-  fem = 0 #this matches males to females by putting the first column always as females
-  
-  #match those of opposite sex with replacement*
-  mates <- sample(pop[pop[,'sex'] != fem, 'id'], tabulate(match(pop[,'sex'], fem)), replace=TRUE) #this means indv can mate more than once
-  #for now, replace = true since sometimes more males than females
-  #note, != means NOT 
-  
-  #pair individuals
-  pairs <- cbind(pop[pop[,'sex'] == fem, 'id'], mates)
-  
-  #randomize the pairs so all indv have chance of mating
-  rand = sample(1:nrow(pairs),nrow(pairs))
-  pairs <- pairs[rand, ]
-  
-  #pairs <- rand
-  colnames(pairs) <- c('mom','dad')
-  return(pairs)
+  ck = mean(pop[,'sex']) #<0.5 female, >.5 male
+  return(ck)
+  if(ck == 1){
+    print(paste("Only males left"))
+    next
+  }else if(ck == 0){
+    print(paste("Only females left"))
+    next
+  }else{
+    #REMOVED###turn "sex" into the value of sex with the fewest indv, 0=female, 1=male
+    #REMOVED###sex <- c(0,1)[which.min(tabulate(match(pop[,'sex'], c(0,1))))]
+    
+    fem = 0 #this matches males to females by putting the first column always as females
+    
+    #match those of opposite sex with replacement*
+    mates <- sample(pop[pop[,'sex'] != fem, 'id'], tabulate(match(pop[,'sex'], fem)), replace=TRUE) #this means indv can mate more than once
+    #for now, replace = true since sometimes more males than females
+    #note, != means NOT 
+    
+    #pair individuals
+    pairs <- cbind(pop[pop[,'sex'] == fem, 'id'], mates)
+    
+    #randomize the pairs so all indv have chance of mating
+    rand = sample(1:nrow(pairs),nrow(pairs))
+    pairs <- pairs[rand, ]
+    
+    #pairs <- rand
+    colnames(pairs) <- c('mom','dad')
+    return(pairs)
+  }
 }
 #NOTES FROM JANNA 10/19/2021
 #try this flow: get all females that are mature, randomize them, then match them with males
