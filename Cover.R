4d8bb3e5c2be0b3b9003335d2da30f9c5e2c9ee1
ginalamka <72942258+ginalamka@users.noreply.github.com>
2021-11-08 14:02:31 -0600

    Add Notes
    added in additional notes from meeting with Janna. this will be used to further mend the simulation.
    
    Other things to rememebr-- using Vortex as pattern matching OR use it for krat pedigree data. OR use slim (whole genomes) after bisulf seq

--- Cover.R
+++ Cover.R
@@ -9,7 +9,7 @@ source(paste(directory, "/Source/FunctionSourcer.R", sep = ''))
 #parameters
 k.V           = 1000          #carrying capacity
 allele.V      = c(0,1)        #alleles assigned to individuals, equal probability of each at all loci, A = 1, a = 0
-nSNP.V        = 10          #number of SNPs simulated
+nSNP.V        = 10            #number of SNPs simulated
 nMicro.V      = 100           #number of microsats simulated
 sex.V         = c(0,1)        #males and females in population, 0 = Female, 1 = Male
 maxage.V      = 3             #maximum age individuals can be; add one at first, so they will die at 4, start at -1
@@ -52,3 +52,14 @@ for(r in 1:nrow(parameters)){
     #lifespan (years)                1.971 and 1.986
     #juvenile offspring              3.149 and 3.171
     #lifetime reproductive success   4.662 nd 4.707
+
+#notes 11/8
+#problem with pop crashing after 6 years is because I am killing too much
+#therefore, instead of random death, put in a age-related increase of chances of death
+#maybe will want to make sure that only adults are mating, not babies -- important for age at maturity -- make temp pop to make sure that only adults are in pop
+#age/lifespan for all individyuals to get this age/random mortality
+    #could do a loop or could use the apply function
+    #apply = give data, apply this function over all rows or all columns. can use apply then sample from 0-1
+    #make sure that the values spit it out corrrectly, i.e. kills 30% not 70$ based on incorrect putting in probabilities
+
+#for the column alive v dead, make sure that the functions taht need pop get a temp pop for only adults (mate choice, breeding) but hten make sure to add the indv (breed) to the main pop, not temp pop
