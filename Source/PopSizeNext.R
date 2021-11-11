0ce13cd4a92edd52779f96a8186297393fd36e3f
ginalamka <72942258+ginalamka@users.noreply.github.com>
2021-11-10 18:42:16 -0600

    Update PopSizeNext.R
    adjust so that popsizenext only calculates from alive indv. consider if babies should count in population growth rates

--- Source/PopSizeNext.R
+++ Source/PopSizeNext.R
@@ -2,9 +2,16 @@
 #this will examine the population size and trend for the future generation. 
 #this will also help in giving the number of offspring to generate from mating pairs
 
-PopSizeNext = function(pop, k, r0){
+PopSizeNext = function(pop, k, r0, maturity){
+  dead = pop[pop[,8] == 0, , drop=FALSE]                 #remove dead indv
+  alive = pop[-which(pop[,1]%in%dead),]
+  #since only returning numboff, dont need to rbind dead and alive into pop
+  #pop = pop[pop[,4] >= maturity, , drop=FALSE]          #isolate adults -- if only want effective pop size to count here
+  #if only using adults, remove immature
+  
   #calculate the current population size following aging and death
-  Nt = nrow(pop)
+  ###REMOVED# Nt = nrow(pop) #use this if removing dead from pop
+  Nt = nrow(alive)
   
   #calculate the new pop size with the logistic growth equation
   Ntt = Nt*(1+r0*(1-(Nt/k))) #logistic
