bc0829de3a52742cf993c07b48f1c07a0681a4b6
ginalamka <72942258+ginalamka@users.noreply.github.com>
2021-11-18 13:20:17 -0600

    Update Migrate.R

--- Source/Migrate.R
+++ Source/Migrate.R
@@ -5,10 +5,11 @@ Migrate = function(pop, source){
   #select number of migrants, from 1-5
   mig = sample(c(1:5), 1, replace=T)
   
+  print(paste("there are", mig, "migrants this year"))
+  
   for(m in 1:mig){
     #select migrant without replacement
     migrant = sample(1:nrow(source), 1, replace = F)
-    
     
     #take migrant from source and put into pop
     pop = rbind(pop, source[migrant,])
