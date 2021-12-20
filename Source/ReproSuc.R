e10ddf24a15bdb4294d7b73a73c7f2b7880ed041
ginalamka <72942258+ginalamka@users.noreply.github.com>
2021-12-20 11:28:49 -0600

    Update ReproSuc.R
    still need to think more about what RRS is and how to calculate. read more into this before getting frustrated lol

--- Source/ReproSuc.R
+++ Source/ReproSuc.R
@@ -2,7 +2,7 @@
 #flow: calc total number of times being a parent > calc how many survive greater than 1 year > then in writeout figure out the rest??
 
 #designated location for this will be in RunModel.R, after runing a replicate
-#data object to use will be FINAL, which is the focal population after y years. contains indv-level data for all
+#data object to use will be pop, which is the focal population after y years. contains indv-level data for all
 
 ReproSuc = function(pop){
   moms = pop[pop[,1] == moms, , drop = FALSE]
@@ -11,5 +11,22 @@ ReproSuc = function(pop){
   #pop1 = pop[-which(pop[,4] >= maxage),]
   
   moms = pop[,2]
-  moms = pop[-which(pop[,1] == moms),]
+  
+  for(q in unique(pop[,2])){
+    sub <- pop[pop[,2] == q,]
+    for(i in unique(pop[,1])){
+      temp <- pop[pop[,1]==i,]
+      print()
+    }
+  }
+
 }
+
+
+for(q in unique(theEND[,8])){
+  temp <- theEND[theEND[,8] ==q,]
+  for(i in unique(theEND[,17])){  
+    sub <- theEND[theEND[,17] == i,]
+    lines(sub[,1], sub[,2], lwd=2)
+    lines(sub[,1], sub[,7], col="blue", lwd=2)
+  }
