5fba1c06359074bec0b9508b3dbd7ca3818fab3c
ginalamka <72942258+ginalamka@users.noreply.github.com>
2021-12-28 12:45:04 -0600

    Update AgeUp.R
    noticed that the dead kept aging. this should stop that from happening. note that sometimes there are age 10 indv. figure out why that might be and fix if needed. is it biologically possible to have a few indv go over the maxage? not sure. consider implications of this

--- Source/AgeUp.R
+++ Source/AgeUp.R
@@ -1,13 +1,25 @@
 #AgeUp
 
 AgeUp = function(pop){
+  dead = pop[pop[,8] == 0, , drop=FALSE]          #remove dead indvs
+  
+  if(nrow(dead)>= 1){
+    pop = pop[-which(pop[,1]%in%dead), , drop=FALSE]
+  }
   
   if(!is.null(nrow(pop))){
     pop[,4] = pop[,4] + 1
-    return(pop)
+    
   }
+  pop<- rbind(pop,dead)
+  remove(dead)
+  return(pop)
 }
 
 #testing
 ## AgeUp(pop)
 
+#must remove dead before aging -- taken from MateChoice.R
+
+#12/28/2021
+#note that sometimes the age is 10. check how that can be and what might need to be done to fix that. 
