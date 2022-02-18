ebd44fca9436f63dfe5a110e0920b7725184aa64
ginalamka <72942258+ginalamka@users.noreply.github.com>
2022-02-18 13:56:27 -0600

    Update Cover.R
    add mu

--- Cover.R
+++ Cover.R
@@ -37,8 +37,9 @@ replicates    = 5 #10
 plotit        = 1    #1=yes, 0=no
 r             = 1
 stoch         = 0    #2=stairstep decrease every year, 1=dramatic drop in k, 0=no
-mutate        = 0    #1=yes, 0=no   #average mammalian genome mutation rate is 2.2 x 10^-9 per base pair per year, https://doi.org/10.1073/pnas.022629899
+mutate        = 1    #1=yes, 0=no   #average mammalian genome mutation rate is 2.2 x 10^-9 per base pair per year, https://doi.org/10.1073/pnas.022629899
 #krats = 2844.77 MB = 2844770000 bp x 2.2*10-9  = 6.258494 === does this matter here??? 
+mu            = 0.001  #mutation rate
 
 #run model iterating over parameters 
 theEND = NULL
