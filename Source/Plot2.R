964e1d0f4871af094a337eedf504e447eecf5fa5
ginalamka <gfl0003@auburn.edu>
2022-05-05 13:27:26 -0500

    Add unique parameter sets

--- Source/Plot2.R
+++ Source/Plot2.R
@@ -2,8 +2,9 @@
 
 Plot2 = function(repEND){
   #set up to subset by replicate and by year born (-1 = migrants, 0 = init focal pop)
-  rep = repEND[, (ncol(repEND))]
-  yr = unique(repEND[, 1])
+  rep = repEND[,9]                 #grabs replicate 
+  par = repEND[,10]                #grabs parameter set
+  yr = unique(repEND[,1])          #grabs unique year values
   mxLRS = ceiling(max(repEND[,3])) #rounds up the maximum LRS
   mxRRS = ceiling(max(repEND[,7])) #rounds up the maximum RRS
   mxindv= ceiling(max(repEND[,2])) #rounds up the maximum number of indv born
@@ -15,17 +16,20 @@ Plot2 = function(repEND){
     
     #LRS over time
     plot(-100, -100 , xlab="generation born", ylab="LRS", xlim=c(min(yr), max(yr)), ylim=c(0, (mxLRS+mxSDL))) 
-    for(i in unique(rep)){  #this allows each rep to be a dif line rather than the lines through it. DO THIS FOR ALL PLOTS
-      temp <- repEND[rep == i,] #create temp matrix for each unique replicate
-      LRS <- temp[,3]
-      lines(yr, LRS, lwd=2) #points(sub[,1], Ho, lwd=2)
-      sd <- temp[,4]
-      uppersd = LRS + sd
-      lowersd = LRS - sd
-      lines(yr, uppersd, lwd=1)
-      lines(yr, lowersd, lwd=1)
-      polygon(x = c(yr, rev(yr)), y = c(uppersd, rev(lowersd)), col = "lightblue", density = 40, angle = 90)
+    for(p in unique(par)){
+      for(i in unique(rep)){  #this allows each rep to be a dif line rather than the lines through it. DO THIS FOR ALL PLOTS
+        temp <- repEND[rep == i,] #create temp matrix for each unique replicate
+        LRS <- temp[,3]
+        lines(yr, LRS, lwd=2) #points(sub[,1], Ho, lwd=2)
+        sd <- temp[,4]
+        uppersd = LRS + sd
+        lowersd = LRS - sd
+        lines(yr, uppersd, lwd=1)
+        lines(yr, lowersd, lwd=1)
+        polygon(x = c(yr, rev(yr)), y = c(uppersd, rev(lowersd)), col = "lightblue", density = 40, angle = 90)
+      }
     }
+    
     #lines(theEND[,1], Ho , xlab="time (years)", ylab="observed heterozygosity", cex = 2, lty = 1, col="black", lwd=5)
     dev.copy(png, "../Output/LRS_over_time.png")
     dev.off()
@@ -33,17 +37,20 @@ Plot2 = function(repEND){
     
     #RRS over time
     plot(-100, -100 , xlab="generation born", ylab="RRS", xlim=c(min(yr), max(yr)), ylim=c(0, (mxRRS+mxSDR))) 
-    for(i in unique(rep)){  #this allows each rep to be a dif line rather than the lines through it. DO THIS FOR ALL PLOTS
-      temp <- repEND[rep == i,] #create temp matrix for each unique replicate
-      RRS <- temp[,7]
-      lines(yr, RRS, lwd=2) #points(sub[,1], Ho, lwd=2)
-      sd <- temp[,8]
-      uppersd = RRS + sd
-      lowersd = RRS - sd
-      lines(yr, uppersd, lwd=1)
-      lines(yr, lowersd, lwd=1)
-      polygon(x = c(yr, rev(yr)), y = c(uppersd, rev(lowersd)), col = "lightpink", density = 40, angle = 45)
+    for(p in unique(par)){
+      for(i in unique(rep)){  #this allows each rep to be a dif line rather than the lines through it. DO THIS FOR ALL PLOTS
+        temp <- repEND[rep == i,] #create temp matrix for each unique replicate
+        RRS <- temp[,7]
+        lines(yr, RRS, lwd=2) #points(sub[,1], Ho, lwd=2)
+        sd <- temp[,8]
+        uppersd = RRS + sd
+        lowersd = RRS - sd
+        lines(yr, uppersd, lwd=1)
+        lines(yr, lowersd, lwd=1)
+        polygon(x = c(yr, rev(yr)), y = c(uppersd, rev(lowersd)), col = "lightpink", density = 40, angle = 45)
+      }
     }
+    
     #lines(theEND[,1], Ho , xlab="time (years)", ylab="observed heterozygosity", cex = 2, lty = 1, col="black", lwd=5)
     dev.copy(png, "../Output/RRS_over_time.png")
     dev.off()
@@ -52,12 +59,14 @@ Plot2 = function(repEND){
     
     #LRS for females vs males
     #observed vs expected hetero
-    plot(-100, -100 , xlab="male LRS", ylab="female LRS", xlim=c(0, 1), ylim=c(0, 1)) 
-    males     = repEND[,6]
-    females   = repEND[,5]
-    for(i in unique(rep)){  #this allows each rep to be a dif line rather than the lines through it. DO THIS FOR ALL PLOTS
-      sub <- repEND[rep == i,] #unique replicate
-      points(males, females, lwd=2)
+    plot(-100, -100 , xlab="male LRS", ylab="female LRS", xlim=c(0, mxLRS), ylim=c(0, mxLRS)) 
+    for(p in unique(par)){
+      for(i in unique(rep)){  #this allows each rep to be a dif line rather than the lines through it. DO THIS FOR ALL PLOTS
+        sub <- repEND[rep == i,] #unique replicate
+        males <- sub[,6]
+        females <- sub[,5]
+        points(males, females, lwd=2)
+      }
     }
     abline(coef = c(0,1), col = "red")
     #points(He, Ho , xlab="expected heterozygosity", ylab="observed heterozygosity", cex = 1, lty = 1, col="black", lwd=5)
@@ -67,16 +76,18 @@ Plot2 = function(repEND){
     #LRS for males vs females over time
     par(mar = c(5,4,4,4)+0.3)
     plot(-100, -100 , xlab="generation born", ylab="female LRS", xlim=c(min(yr), max(yr)), ylim=c(0, mxLRS)) 
-    for(i in unique(rep)){  #this allows each rep to be a dif line rather than the lines through it. DO THIS FOR ALL PLOTS
-      sub <- repEND[rep == i,]  #unique replicate
-      females = sub[,5]
-      lines(yr, females, lwd=2, col = "pink")
-    }
-    par(new = TRUE)
-    for(i in unique(rep)){  #this allows each rep to be a dif line rather than the lines through it. DO THIS FOR ALL PLOTS
-      sub <- repEND[rep == i,] #unique replicate
-      males = sub[,6]
-      lines(yr, males, lwd=2, col = "blue")
+    for(p in unique(par)){
+      for(i in unique(rep)){  #this allows each rep to be a dif line rather than the lines through it. DO THIS FOR ALL PLOTS
+        sub <- repEND[rep == i,]  #unique replicate
+        females = sub[,5]
+        lines(yr, females, lwd=2, col = "pink")
+      }
+      par(new = TRUE)
+      for(i in unique(rep)){  #this allows each rep to be a dif line rather than the lines through it. DO THIS FOR ALL PLOTS
+        sub <- repEND[rep == i,] #unique replicate
+        males = sub[,6]
+        lines(yr, males, lwd=2, col = "blue")
+      }
     }
     axis(side = 4, at = NULL, labels = TRUE, col = "blue")
     mtext("male LRS", side=4, line =3, col = "blue")
@@ -85,14 +96,17 @@ Plot2 = function(repEND){
     
     #number indv born in per generation
     plot(-100, -100 , xlab="generation born", ylab="number of babies", xlim=c(min(yr), max(yr)), ylim=c(0, mxindv)) 
-    for(i in unique(rep)){  #this allows each rep to be a dif line rather than the lines through it. DO THIS FOR ALL PLOTS
-      temp <- repEND[rep == i,] #create temp matrix for each unique replicate
-      born <- temp[,2]
-      lines(yr, born, lwd=2) #points(sub[,1], Ho, lwd=2)
+    for(p in unique(par)){
+      for(i in unique(rep)){  #this allows each rep to be a dif line rather than the lines through it. DO THIS FOR ALL PLOTS
+        temp <- repEND[rep == i,] #create temp matrix for each unique replicate
+        born <- temp[,2]
+        lines(yr, born, lwd=2) #points(sub[,1], Ho, lwd=2)
+      }
     }
     #lines(theEND[,1], Ho , xlab="time (years)", ylab="observed heterozygosity", cex = 2, lty = 1, col="black", lwd=5)
     dev.copy(png, "../Output/nindv_born_per_generation.png")
     dev.off()
+    #NOTE: will want to remove years -1 and 0 probs
     
   }
 }
