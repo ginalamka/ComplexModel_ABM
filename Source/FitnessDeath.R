76e9c901664bc399332e2fae7386ef29e97cabc8
ginalamka <72942258+ginalamka@users.noreply.github.com>
2022-09-21 12:22:46 -0500

    Create FitnessDeath.R
    this gives an increasing probability of dying at age of maturity when heterozygosity decreases. this is placed only at age = maturity so that the fitness influence is not compounded as the indv ages

---
+++ Source/FitnessDeath.R
@@ -0,0 +1,47 @@
+#Fitness-Dependent death
+#taking the place of Death
+#used for complex model for ABM class 2021
+
+#this will impose an increase in probability of death wiht decreasing heterozygosity
+FitnessDeath = function(pop, maturity, ratemort, y){
+  dead = pop[pop[,8] == 0, , drop=FALSE]          #remove dead indvs
+  
+  pop = pop[pop[,8] == 1, , drop=FALSE]           #isolate alive
+  
+  mature = pop[pop[,4] == maturity, , drop=FALSE] #isolate the indv that just reached maturity
+  #influcing fitness-induced death at maturity helps control for fitness influences better than if compounded every year
+  pop = pop[pop[,4] != maturity, , drop=FALSE]
+  
+  if(nrow(mature)>1){
+    for(ee in 1:nrow(mature)){
+      
+      het = mature[ee,11] +0.01 #this controls for if het =0
+      mature[ee,8] = sample(x=c(0,1), size = 1, replace = TRUE, prob = c(1/het/100,(1-(1/het/100))))
+        
+        #notes 9/16/22 - do this only at the year of maturity-- ONCE because the fitness effect that happens every year would be stronger if it compounded
+        #if you have the maxhet change over time, the probability of dying would change each year so it would be hard to measure.
+        # look for the realtionship of heterozyogisty to probability of death to see if there is stuff on there (might not be)
+        #do oldies > fitness at maturity age > age-based death (age/lifespan) === DO NOT PUT A LIMIT ON THE NUMBER TO KILL
+        #
+     }
+     nkilled = mature[mature[,8]==0,,drop=FALSE]
+     print(paste("killed", nrow(nkilled), "individuals"))
+      
+    }else{
+      next #skips to next function if there arent any indv that reach maturity this year
+    }
+
+
+  #combine pop and previously removed dead indv
+  pop = rbind(pop, mature, dead)
+  
+  #if(nrow(alreadydead) >= 1){
+    #pop = rbind(pop,alreadydead)
+  #}
+  
+  return(pop)
+}
+
+
+##DOUBLE CHECK THAT NOT LOSING INDVS
+#RARELY, but still a thing, a few individuals die that shouldnt (i.e. they are not at max age and are killed with oldies but the number to kill is smaller than oldies, so that shouldnt happen). keep and eye on this and figure out why this occurs
