bde36ae6d24206f030c56e37b64ed23be4fb16ee
ginalamka <72942258+ginalamka@users.noreply.github.com>
2022-12-08 14:44:20 -0600

    archived retired scripts

---
+++ Archive/RetiredScripts/RandomDeath.R
@@ -0,0 +1,21 @@
+#Random Death
+#random death for Complex Migration Model for ABM class Fall 2021 
+## NOTE - this is currently RANDOM, but condider imposing a fitness/heterozygosity cost to imply natural selection
+## consider implications of a no selection model and a "natural selection" model. 
+
+RandomDeath = function(pop){
+  #randomly select indv to die
+  #syntax: sample(group, X, replace) with group being the column in the matrix, X being the number to kill per run
+  #the number of indv to die will vary, so calibrate this later
+  
+  dead = sample(pop[,1], (nrow(pop)*0.1), replace = FALSE) #this is to kill off 10% of the population size randomly
+  pop1 = pop[-which(pop[,1]%in%dead),] 
+  pop <- pop1
+  
+  return(pop)
+}
+
+#to do the opposite and randomly select those that live (if 98 out of 100 indv), do this:
+#alive = sample(pop[,1], 98, replace = FALSE)
+
+#note that instead of %in%, if only killing ONE indv, can use == ; but since we are killing a list of indv, must use %in% or %NOTin%
