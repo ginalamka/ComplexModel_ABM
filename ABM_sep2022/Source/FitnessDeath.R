d9b61a5e1572dd031635acb439eb0c0a5d838952
ginalamka <72942258+ginalamka@users.noreply.github.com>
2024-01-30 13:07:25 -0600

    remove het fitness death

--- ABM_sep2022/Source/FitnessDeath.R
+++ ABM_sep2022/Source/FitnessDeath.R
@@ -2,36 +2,39 @@
 #this will impose an increase in probability of death with decreasing heterozygosity
 
 FitnessDeath = function(pop, maturity, y, purge, nSNP.cons){
-  print(paste("nrow(pop)", nrow(pop), "sum(pop[,8]", sum(pop[,8])))
+  print(paste("nrow(pop)", nrow(pop), "sum(pop[,8])", sum(pop[,8])))
   dead = pop[pop[,8] == 0, , drop=FALSE]            #remove dead indvs
   print(paste("nrow(dead)", nrow(dead)))
+  
   pop = pop[pop[,8] == 1, , drop=FALSE]             #isolate alive
   print(paste("nrow(pop)", nrow(pop)))
   #adults = pop[pop[,4] >= maturity, , drop=FALSE]   #grab adults
   #n_adults = nrow(adults)
-  table(pop[,4])
   print(paste(table(pop[,4])))
+  
   mature = pop[pop[,4] == maturity, , drop=FALSE]   #isolate the indv that just reached maturity
   print(paste("nrow(mature)", nrow(mature)))
   #influencing fitness-induced death once at the year of maturity helps control for fitness influences better than if compounded every year indv ages
+  
   pop = pop[pop[,4] != maturity, , drop=FALSE]
   print(paste("nrow(pop)", nrow(pop)))
-  if(nrow(mature)>1){
-    for(ee in 1:nrow(mature)){
-      het = mature[ee,11] +0.01                     #this controls for if het = 0
-      mature[ee,8] = sample(x=c(0,1), size = 1, replace = TRUE, prob = c(1/het/100,(1-(1/het/100))))
-    }
-    nkilled = NULL
-    nkilled = mature[mature[,8]==0,,drop=FALSE]    #grab dead mature indvs
-    mature = mature[mature[,8]!=0,,drop=FALSE]     #grab alive mature indvs
-    nkilled[,18] = 2
-    nkilled[,10] = y
-    print(paste("killed", nrow(nkilled), "individuals"))
-    
-    }else{
-      print(paste("not enough mature for fit-induced death"))
-      nkilled = NULL
-    }
+  
+  # if(nrow(mature)>1){
+  #   for(ee in 1:nrow(mature)){
+  #     het = mature[ee,11] +0.01                     #this controls for if het = 0
+  #     mature[ee,8] = sample(x=c(0,1), size = 1, replace = TRUE, prob = c(1/het/100,(1-(1/het/100))))
+  #   }
+  #   nkilled = NULL
+  #   nkilled = mature[mature[,8]==0,,drop=FALSE]    #grab dead mature indvs
+  #   mature = mature[mature[,8]!=0,,drop=FALSE]     #grab alive mature indvs
+  #   nkilled[,18] = 2
+  #   nkilled[,10] = y
+  #   print(paste("killed", nrow(nkilled), "individuals"))
+  #   
+  #   }else{
+  #     print(paste("not enough mature for fit-induced death"))
+  #     nkilled = NULL
+  #   }
   
   if(purge == 1){
     if(purge_mutants == 1){   #if want to grab only mutants
@@ -64,11 +67,11 @@ FitnessDeath = function(pop, maturity, y, purge, nSNP.cons){
     
   }else{npurged = NULL}
   
-  print(paste(nrow(pop), nrow(mature), nrow(npurged), nrow(nkilled), nrow(dead)))
+  print(paste(nrow(pop), nrow(mature), nrow(npurged), nrow(dead))) #nrow(nkilled), 
   
-  pop = rbind(pop, mature, npurged, nkilled, dead)  #combine pop, mature, and previously removed dead indv
+  pop = rbind(pop, mature, npurged, dead)  #combine pop, mature, and previously removed dead indv #nkilled, 
   
-  remove(dead, mature, npurged, nkilled)   #clean up
+  remove(dead, mature, npurged)   #clean up #, nkilled
   
   return(pop)
 }
